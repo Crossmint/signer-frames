d7ac9850e93347ba2ab414bc6b14e89966dd4534
Alberto Elias <hi@albertoelias.me>
2025-04-24 10:48:01 -0400

    support webview as well

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,54 +1,69 @@
-import { HandshakeChild, type HandshakeOptions } from '@crossmint/client-sdk-window';
-import { signerInboundEvents, signerOutboundEvents } from '@crossmint/client-signers';
+import {
+	ChildWindow,
+	type HandshakeChild,
+	type HandshakeOptions,
+	RNWebViewChild,
+} from "@crossmint/client-sdk-window";
+import {
+	signerInboundEvents,
+	signerOutboundEvents,
+} from "@crossmint/client-signers";
 
 const EVENT_VERSION = 1;
 
 export class EventsService {
-  private static messenger: HandshakeChild<
-    typeof signerInboundEvents,
-    typeof signerOutboundEvents
-  > | null = null;
+	private static messenger: HandshakeChild<
+		typeof signerInboundEvents,
+		typeof signerOutboundEvents
+	> | null = null;
 
-  /**
-   * Initialize the messenger and register event handlers
-   * The messenger is shared across all instances
-   */
-  async initMessenger(options?: {
-    handshakeOptions?: HandshakeOptions;
-    targetOrigin?: string;
-  }): Promise<void> {
-    if (EventsService.messenger) {
-      console.log('Messenger already initialized');
-      return;
-    }
+	/**
+	 * Initialize the messenger and register event handlers
+	 * The messenger is shared across all instances
+	 */
+	async initMessenger(options?: {
+		handshakeOptions?: HandshakeOptions;
+		targetOrigin?: string;
+	}): Promise<void> {
+		if (EventsService.messenger) {
+			console.log("Messenger already initialized");
+			return;
+		}
 
-    EventsService.messenger = new HandshakeChild(window.parent, '*', {
-      incomingEvents: signerInboundEvents,
-      outgoingEvents: signerOutboundEvents,
-      handshakeOptions: options?.handshakeOptions,
-      targetOrigin: options?.targetOrigin,
-    });
+		EventsService.messenger =
+			"ReactNativeWebView" in window && window.ReactNativeWebView != null
+				? new RNWebViewChild({
+						incomingEvents: signerInboundEvents,
+						outgoingEvents: signerOutboundEvents,
+					})
+				: new ChildWindow(window.parent, options?.targetOrigin ?? "*", {
+						incomingEvents: signerInboundEvents,
+						outgoingEvents: signerOutboundEvents,
+						handshakeOptions: options?.handshakeOptions,
+					});
 
-    await EventsService.messenger.handshakeWithParent();
-  }
+		await EventsService.messenger.handshakeWithParent();
+	}
 
-  /**
-   * Assert that the messenger is initialized and connected
-   */
-  private assertMessengerInitialized(): void {
-    if (!EventsService.messenger) {
-      throw new Error('Messenger not initialized');
-    }
-    if (!EventsService.messenger.isConnected) {
-      throw new Error('Messenger not connected');
-    }
-  }
+	/**
+	 * Assert that the messenger is initialized and connected
+	 */
+	private assertMessengerInitialized(): void {
+		if (!EventsService.messenger) {
+			throw new Error("Messenger not initialized");
+		}
+		if (!EventsService.messenger.isConnected) {
+			throw new Error("Messenger not connected");
+		}
+	}
 
-  /**
-   * Get the messenger instance
-   */
-  getMessenger(): NonNullable<typeof EventsService.messenger> {
-    this.assertMessengerInitialized();
-    return EventsService.messenger as NonNullable<typeof EventsService.messenger>;
-  }
+	/**
+	 * Get the messenger instance
+	 */
+	getMessenger(): NonNullable<typeof EventsService.messenger> {
+		this.assertMessengerInitialized();
+		return EventsService.messenger as NonNullable<
+			typeof EventsService.messenger
+		>;
+	}
 }
