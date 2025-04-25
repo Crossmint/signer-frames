51b26f2d4e211b37df279a114ffd4db0cb6bc0c2
Alberto García Planes <alberto@paella.dev>
2025-04-25 13:54:00 +0200

    Fix format

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,67 +1,62 @@
 import {
-	ChildWindow,
-	type HandshakeChild,
-	type HandshakeOptions,
-	RNWebViewChild,
-} from "@crossmint/client-sdk-window";
-import {
-	signerInboundEvents,
-	signerOutboundEvents,
-} from "@crossmint/client-signers";
+  ChildWindow,
+  type HandshakeChild,
+  type HandshakeOptions,
+  RNWebViewChild,
+} from '@crossmint/client-sdk-window';
+import { signerInboundEvents, signerOutboundEvents } from '@crossmint/client-signers';
 
 export class EventsService {
-	private static messenger: HandshakeChild<
-		typeof signerInboundEvents,
-		typeof signerOutboundEvents
-	> | null = null;
+  private static messenger: HandshakeChild<
+    typeof signerInboundEvents,
+    typeof signerOutboundEvents
+  > | null = null;
 
-	/**
-	 * Initialize the messenger and register event handlers
-	 * The messenger is shared across all instances
-	 */
-	async initMessenger(options?: {
-		handshakeOptions?: HandshakeOptions;
-		targetOrigin?: string;
-	}): Promise<void> {
-		if (EventsService.messenger) {
-			console.log("Messenger already initialized");
-			return;
-		}
+  /**
+   * Initialize the messenger and register event handlers
+   * The messenger is shared across all instances
+   */
+  async initMessenger(options?: {
+    handshakeOptions?: HandshakeOptions;
+    targetOrigin?: string;
+  }): Promise<void> {
+    if (EventsService.messenger) {
+      console.log('Messenger already initialized');
+      return;
+    }
 
-		EventsService.messenger =
-			"ReactNativeWebView" in window && window.ReactNativeWebView != null
-				? new RNWebViewChild({
-						incomingEvents: signerInboundEvents,
-						outgoingEvents: signerOutboundEvents,
-					})
-				: new ChildWindow(window.parent, options?.targetOrigin ?? "*", {
-						incomingEvents: signerInboundEvents,
-						outgoingEvents: signerOutboundEvents,
-						handshakeOptions: options?.handshakeOptions,
-					});
+    EventsService.messenger =
+      'ReactNativeWebView' in window && window.ReactNativeWebView != null
+        ? new RNWebViewChild({
+            incomingEvents: signerInboundEvents,
+            outgoingEvents: signerOutboundEvents,
+          })
+        : new ChildWindow(window.parent, options?.targetOrigin ?? '*', {
+            incomingEvents: signerInboundEvents,
+            outgoingEvents: signerOutboundEvents,
+            handshakeOptions: options?.handshakeOptions,
+          });
 
-		await EventsService.messenger.handshakeWithParent();
-	}
+    await EventsService.messenger.handshakeWithParent();
+  }
 
-	/**
-	 * Assert that the messenger is initialized and connected
-	 */
-	private assertMessengerInitialized(): void {
-		if (!EventsService.messenger) {
-			throw new Error("Messenger not initialized");
-		}
-		if (!EventsService.messenger.isConnected) {
-			throw new Error("Messenger not connected");
-		}
-	}
+  /**
+   * Assert that the messenger is initialized and connected
+   */
+  private assertMessengerInitialized(): void {
+    if (!EventsService.messenger) {
+      throw new Error('Messenger not initialized');
+    }
+    if (!EventsService.messenger.isConnected) {
+      throw new Error('Messenger not connected');
+    }
+  }
 
-	/**
-	 * Get the messenger instance
-	 */
-	getMessenger(): NonNullable<typeof EventsService.messenger> {
-		this.assertMessengerInitialized();
-		return EventsService.messenger as NonNullable<
-			typeof EventsService.messenger
-		>;
-	}
+  /**
+   * Get the messenger instance
+   */
+  getMessenger(): NonNullable<typeof EventsService.messenger> {
+    this.assertMessengerInitialized();
+    return EventsService.messenger as NonNullable<typeof EventsService.messenger>;
+  }
 }
